e67af3e07eef9cf4d0ffb5e2334431e8ac0ee7f9
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-10 19:20:47 +0530

    [Afrath] Updated the file with Finding frequency of words in sentence

---
+++ Hashing/MyMapNode.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    //structure
+    public struct KeyValue<K, V>
+    {
+        public K Key { set; get; }
+        public V Value { set; get; }
+    }
+    class MyMapNode<K,V>
+    {
+        int size;
+        LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+
+        }
+        public int GetBucketValue(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public LinkedList<KeyValue<K,V>> GetLinkedListPosition(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if(linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+        public int CheckFreq(K key)
+        {
+            int position = GetBucketValue(key);
+            LinkedList<KeyValue<K, V>> list = GetLinkedListPosition(position);
+            int count = 1;
+            bool alreadyfound = false;
+            KeyValue<K, V> alreadyfoundKey = default(KeyValue<K,V>);
+            foreach (KeyValue<K, V> keyValue in list)
+            {
+                if (keyValue.Key.Equals(key))
+                {
+                    count = Convert.ToInt32(keyValue.Value) + 1;
+                    alreadyfound = true;
+                    alreadyfoundKey = keyValue;
+                }
+            }
+            if (alreadyfound)
+            {
+                list.Remove(alreadyfoundKey);
+                return count;
+            }
+            else
+            {
+                return 1;
+            }
+
+        
+
+    }
+        public void Add(K key, V value)
+        {
+            int position = GetBucketValue(key);
+            LinkedList<KeyValue<K, V>> list = GetLinkedListPosition(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key,Value=value};
+            list.AddLast(item);
+
+        }
+        public V Get(K key)
+        {
+            int position = GetBucketValue(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedListPosition(position);
+            foreach(KeyValue<K,V> keyValue in linkedlist)
+            {
+                if(keyValue.Key.Equals(key))
+                {
+
+                    return keyValue.Value;
+                }
+            }
+            return default(V);
+        }
+
+
+    }
+
+    
+}
--- Hashing/Program.cs
+++ Hashing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Hashing
 {
@@ -7,6 +8,31 @@ namespace Hashing
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to hashing");
+            string sample = "To be or not to be";
+            string[] splited = sample.ToLower().Split(' ');
+            HashSet<string> set = new HashSet<string>();
+            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(5);
+            for (int i = 0; i < splited.Length; i++)
+            {
+                int count = myMapNode.CheckFreq(splited[i]);
+                if (count > 1)
+                {
+                    myMapNode.Add(splited[i], count);
+                    set.Add(splited[i]);
+                }
+                else
+                {
+                    myMapNode.Add(splited[i], 1);
+                    set.Add(splited[i]);
+                }
+
+            }
+
+            foreach(var member in set)
+            {
+                Console.WriteLine("The frequency of {0} in sentence is {1}",member,myMapNode.Get(member));
+            }
+
         }
     }
 }
